f0156982a8f24a2efbb500b8c32a0fddf40611fb
DorsaAllahverdi <allahverdi.dorsa@gmail.com>
2024-08-12 16:16:10 +0330

    projectsLayoutCompleted

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Navigation from './Components/Navigation/Navigation';
 import AboutMe from './Components/AboutMe/AboutMe';
 import Projects from './Components/Projects/Projects';
 import ProjectsLayout from './Components/Projects/ProjectsLayout/ProjectsLayout';
-import FaceRecognitionProj from './Components/Projects/FaceRecognitionProj/FaceRecognitionProj';
-import RoboFriendsProj from './Components/Projects/RoboFriendsProj/RoboFriendsProj';
+import FaceRecognition from './Components/Projects/FaceRecognition.png';
+import FaceRecognitionHooks from './Components/Projects/FaceRecognitionHooks.png';
+import RoboFriends from './Components/Projects/RoboFriends.png';
 
 function App() {
   return (
@@ -17,12 +18,24 @@ function App() {
       <Navigation />
       <AboutMe />
       <Projects>
-        <ProjectsLayout>
-          <FaceRecognitionProj />
-        </ProjectsLayout>
-        <ProjectsLayout>
-          <RoboFriendsProj />
-        </ProjectsLayout>
+        <ProjectsLayout
+          projImg={FaceRecognition} 
+          projTitle='Face Recognition App (Using React Class Components)'
+          projDesc="This innovative application, written using the React class components concept, allows users to input an image URL and automatically detects any faces present in the image. Utilizing advanced facial recognition technology, the application identifies multiple faces and highlights each one by drawing a square around it. This feature ensures that all detected faces are clearly marked, making it easy for users to identify individuals within the image. Whether it's a group photo or a crowded scene, the application efficiently processes the image to provide accurate and reliable face detection results."
+          projLiveUrl='https://facerecognitionusingclasses.vercel.app/'
+          projCodeUrl='https://github.com/DorsaAllahverdi/facerecognitionusingclasses' />
+        <ProjectsLayout
+          projImg={FaceRecognitionHooks} 
+          projTitle='Face Recognition App (Using React Hooks)'
+          projDesc="This innovative application, written using the React Hooks concept, allows users to input an image URL and automatically detects any faces present in the image. Utilizing advanced facial recognition technology, the application identifies multiple faces and highlights each one by drawing a square around it. This feature ensures that all detected faces are clearly marked, making it easy for users to identify individuals within the image. Whether it's a group photo or a crowded scene, the application efficiently processes the image to provide accurate and reliable face detection results."
+          projLiveUrl='https://facerecognitionusinghooks.vercel.app/'
+          projCodeUrl='https://github.com/DorsaAllahverdi/facerecognitionusinghooks' />
+        <ProjectsLayout
+          projImg={RoboFriends} 
+          projTitle='RoboFriends App (Using React Hooks)'
+          projDesc="This innovative application, developed using the React Hooks concept, features a collection of robot images, names, and email addresses. Users can easily search for robots by their names, thanks to the efficient search functionality integrated into the app. By leveraging React Hooks, the application manages state and side effects seamlessly, ensuring a smooth and responsive user experience. Whether you're looking for a specific robot or just browsing through the collection, this app provides a user-friendly interface to explore and find the information you need quickly."
+          projLiveUrl='https://robofriends-black.vercel.app/'
+          projCodeUrl='https://github.com/DorsaAllahverdi/robofriends' />
       </Projects>
     </div>
   );
--- src/Components/Projects/ProjectsLayout/ProjectsLayout.js
+++ src/Components/Projects/ProjectsLayout/ProjectsLayout.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import './ProjectsLayout.css';
+import htmllogo from '../html5logo.png';
+import csslogo from '../css3logo.png';
+import jslogo from '../jslogo.png';
+import reactlogo from '../reactlogo.png';
+import live from '../Vercel.svg';
+import git from '../git.png';
 
-const ProjectsLayout = (props) => {
+const ProjectsLayout = ({projImg, projTitle, projDesc, projLiveUrl, projCodeUrl}) => {
     return (
         <div className='madiv flex justify-center'>
             <div className='img-wrap'>
-                {props.children}
+                <div>
+                    <img alt='' src={projImg} />
+                    <div className='img-description'>
+                        <div className='flex overflow-y-auto flex-column proj-description'>
+                            <h3>{projTitle}</h3>
+                            <div>
+                                <img className='logo-size' alt='' src={htmllogo} />
+                                <img className='logo-size' alt='' src={csslogo} />
+                                <img className='logo-size' alt='' src={jslogo} />
+                                <img className='logo-size' alt='' src={reactlogo} />
+                            </div>
+                            <div className='tl'>
+                                <p>
+                                {projDesc}
+                                </p>
+                            </div>
+                        </div>
+                        <div className='flex items-center code-container h-inherit'>
+                            <div className='flex flex-column justify-around code-frame'>
+                                <div className='flex flex-column items-center code-ma'>
+                                    <button className='code-button'>
+                                        <a href={projLiveUrl} target='_self'>
+                                            <img className='logo-size' alt='' src={live} />
+                                        </a>
+                                    </button>
+                                    <p>Live</p>
+                                </div>
+                                <div className='flex flex-column items-center code-ma'>
+                                    <button className='code-button'>
+                                        <a href={projCodeUrl} target='_self'>
+                                            <img className='logo-size' alt='' src={git} /> 
+                                        </a>
+                                    </button>
+                                    <p>Code</p>
+                                </div>   
+                            </div>             
+                        </div>
+                    </div>
+                </div>
             </div>
         </div> 
     );
