e817ab0631353570d44701acff29dc8b3a76aedc
DorsaAllahverdi <allahverdi.dorsa@gmail.com>
2024-08-20 14:24:57 +0330

    aboutmeimagechange

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -8,8 +8,9 @@ const AboutMe = () => {
         <section id='about' className='about-me'>
             <div className='dib relative pa5 ma2'>
             <ScrollComponent>
+
+            <img className='db fl image' alt='' src={myimg} />
                 <h1 className='text'>About Me</h1>
-                    <img className='db fl image' alt='' src={myimg} />
                     <p className='f4 tl text'>
                         My passion for programming began when I was 15 years old with C++, leading me to join our province's RoboCup 2D team. This early experience laid the foundation for my career in technology. I hold a Bachelor's degree in Computer Engineering from BIHE, which, along with my diverse work experience, makes me a skilled developer.
                         <br /><br />As a front-end programmer, I excel in creating seamless and dynamic user interfaces using HTML, CSS, JavaScript, and React. I have successfully completed courses on React and other front-end development skills, which have equipped me with the knowledge to build responsive and user-friendly web applications. Additionally, I have undertaken several projects in this field, further honing my expertise and practical experience.
